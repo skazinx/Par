7c57ee42102a2dcae89b07c23a8017c7295f6ac0
Leonardo <leonardomabreu09@gmail.com>
2025-03-25 00:48:29 -0300

    Corrigindo erro de importação no GitHub Actions

--- tests/test_matematica.py
+++ tests/test_matematica.py
@@ -1,4 +1,10 @@
-from src.matematica import eh_par
+import sys
+import os
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from matematica import eh_par
 
 def test_numeros_pares():
     assert eh_par(2) == True
